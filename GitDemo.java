97eeb64f210a66f0cdb2612729ee8cfdfa0a918b
chenxing0120 <93439647+chenxing0120@users.noreply.github.com>
2022-05-10 15:32:48 +0800

    111

--- GitDemo.java
+++ GitDemo.java
@@ -2,6 +2,5 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello strong Git!!!");
    	System.out.println("Hello World!!!");
-	System.out.println("Hello strong hit-fox");
     }
 }
