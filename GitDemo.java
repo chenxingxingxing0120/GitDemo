5d98d5e01f0807f358c633ec4c9fa02a408e2fba
cx <cx0120@163.com>
2022-05-10 10:11:22 +0800

    version-07

--- GitDemo.java
+++ GitDemo.java
@@ -1,6 +1,6 @@
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello Strong Git!!!");
+        System.out.println("Hello strong Git!!!");
    	System.out.println("Hello World!!!");
 	System.out.println("Hello hit-fox");
     }
